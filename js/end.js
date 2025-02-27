a6101905803ce06de39be4b7be0800d7953b68a0
laiapozo <pozotalarnlaia@gmail.com>
2025-02-27 19:18:37 +0100

    Add Game Over banner when timer is 0

---
+++ js/end.js
@@ -0,0 +1,12 @@
+"use strict";
+// De momento está metido dentro de la función de timer, cuando el tiempo se termina
+// Si lo pongo como está aquí, cuando JS evalua está condición es false, porque al principio gameOver es un párrafo vacío
+
+// const gameOver = document.querySelector(".game-over");
+
+// console.log(gameOver);
+// console.log(timer.textContent);
+
+// if (timer.innerHTML === "00:00") {
+//     gameOver.classList.remove("hidden");
+// }
--- js/timer.js
+++ js/timer.js
@@ -1,5 +1,9 @@
-let timeLimitInSeconds = 105;
+"use strict";
+
+let timeLimitInSeconds = 5;
 const timer = document.querySelector(".timer");
+
+const gameOver = document.querySelector(".game-over");
 
 function startTimer() {
   timeLimitInSeconds--; // Restar segundos continuamente
@@ -8,7 +12,7 @@ function startTimer() {
   let seconds = timeLimitInSeconds % 60; // Por ejemplo con 105 --> 105 = 60 × 1 + 45 (45 son los segundos que se necesitan)
 
   seconds < 10 ? (seconds = "0" + seconds) : seconds; // Para los últimos 9 segundos, poner un 0 delante para que quede tipo 0:05
- 
+
   if (timeLimitInSeconds < 0) {
     // Si los segundos terminan, se cancela la acción y pinta 00:00 en el timer
     clearInterval(timerInterval); // clearInterval() method of the Window interface cancels a timed, repeating action which was previously established by a call to setInterval()
@@ -16,6 +20,10 @@ function startTimer() {
   }
 
   timer.innerHTML = minutes + ":" + seconds; //Se pinta en el timer el tiempo cada segundo, ya que se ejecuta la función cada segundo
+
+  if (timer.innerHTML === "00:00") { //Cuando el timer esté a 0, aparece el banner de Game Over que tenía la clase hidden
+    gameOver.classList.remove("hidden");
+  }
 }
 
 const timerInterval = setInterval(startTimer, 1000); // Method of the Window interface repeatedly calls a function, with a fixed time delay (in milliseconds, 1000 ms = 1s) between each call
