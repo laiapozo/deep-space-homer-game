7c6aa52edb0ed03c529b43f6519b49dbd5ee5185
laiapozo <pozotalarnlaia@gmail.com>
2025-03-08 10:24:39 +0100

    Add collision score

--- js/collision.js
+++ js/collision.js
@@ -1,48 +1,32 @@
 "use strict";
 const checkCollision = () => {
-    // const homerPosition = document.querySelector(".js-homer").getBoundingClientRect();
-    // const chip1Position = document.querySelector(".js-chip1").getBoundingClientRect();
-    // const chip2Position = document.querySelector(".js-chip2").getBoundingClientRect();
-    // const chip3Position = document.querySelector(".js-chip3").getBoundingClientRect();
-    // const chip4Position = document.querySelector(".js-chip4").getBoundingClientRect();
-    // const chip5Position = document.querySelector(".js-chip5").getBoundingClientRect();
-
     const homer = document.querySelector(".js-homer");
-    const chip1 = document.querySelector(".js-chip1")
-
+    const chip1 = document.querySelector(".js-chip1");
+    const chip2 = document.querySelector(".js-chip2");
+    const chip3 = document.querySelector(".js-chip3");
+    const chip4 = document.querySelector(".js-chip4");
+    const chip5 = document.querySelector(".js-chip5");
     const scoreHtml = document.querySelector(".js-score");
     let score =  parseInt(scoreHtml.innerHTML);
-
-    // console.log(homerPosition);
-    // console.log(homer.offsetLeft);
-    // console.log(homer.offsetTop);
-    // console.log(chip1.offsetLeft);
-    // console.log(chip1.offsetTop);
-
-
-    /* 
-    Posicions homi:
-        offsetLeft sempre es: 20
-        offsetTop -->   top1: 14
-                        top2: 97
-                        top3: 180
-                        top4: 263
-                        top5: 346
         
-    Posicions chip1:
-        offsetTop sempre es: 37
-        offsetLeft quan hi ha colission: 125
-
-    */
-
-        
-    if (homer.offsetTop === 14 && chip1.offsetLeft === 125) {
-        console.log("yesss");
+    if (homer.offsetTop === 14 && chip1.offsetLeft < 150 && chip1.offsetLeft > -35) {
+        ++score;
+        scoreHtml.innerHTML = score;
+    } else if (homer.offsetTop === 97 && chip2.offsetLeft < 150 && chip2.offsetLeft > -35) {
+        ++score;
+        scoreHtml.innerHTML = score;
+    } else if (homer.offsetTop === 180 && chip3.offsetLeft < 150 && chip3.offsetLeft > -35) {
+        ++score;
+        scoreHtml.innerHTML = score;
+    } else if (homer.offsetTop === 263 && chip4.offsetLeft < 150 && chip4.offsetLeft > -35) {
+        ++score;
+        scoreHtml.innerHTML = score;
+    } else if (homer.offsetTop === 346 && chip5.offsetLeft < 150 && chip5.offsetLeft > -35) {
         ++score;
         scoreHtml.innerHTML = score;
     }
 }
 
-setInterval(checkCollision, 10);
+setInterval(checkCollision, 1000);
 
 document.addEventListener("DOMContentLoaded", checkCollision);
