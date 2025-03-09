7131a4f777a554ad7791d1eaa475d8b8f17ebe31
laiapozo <pozotalarnlaia@gmail.com>
2025-03-09 19:31:25 +0100

    Fix collision for big screens

--- js/main.js
+++ js/main.js
@@ -75,19 +75,19 @@ const checkCollision = () => {
     const scoreHtml = document.querySelector(".js-score");
     let score =  parseInt(scoreHtml.innerHTML);
         
-    if (homer.offsetTop === 14 && chip1.offsetLeft < 150 && chip1.offsetLeft > -35) {
+    if (homer.offsetTop > 12 && homer.offsetTop < 16 && chip1.offsetLeft < 150 && chip1.offsetLeft > -35) {
         ++score;
         scoreHtml.innerHTML = score;
-    } else if (homer.offsetTop === 97 && chip2.offsetLeft < 150 && chip2.offsetLeft > -35) {
+    } else if (homer.offsetTop > 95 && homer.offsetTop < 99 && chip2.offsetLeft < 150 && chip2.offsetLeft > -35) {
         ++score;
         scoreHtml.innerHTML = score;
-    } else if (homer.offsetTop === 180 && chip3.offsetLeft < 150 && chip3.offsetLeft > -35) {
+    } else if (homer.offsetTop > 178 && homer.offsetTop < 182 && chip3.offsetLeft > -35 && chip3.offsetLeft < 150) {
         ++score;
         scoreHtml.innerHTML = score;
-    } else if (homer.offsetTop === 263 && chip4.offsetLeft < 150 && chip4.offsetLeft > -35) {
+    } else if (homer.offsetTop > 261 && homer.offsetTop < 265 && chip4.offsetLeft > -35 && chip4.offsetLeft < 150) {
         ++score;
         scoreHtml.innerHTML = score;
-    } else if (homer.offsetTop === 346 && chip5.offsetLeft < 150 && chip5.offsetLeft > -35) {
+    } else if (homer.offsetTop > 344 && homer.offsetTop < 348 && chip5.offsetLeft > -35 && chip5.offsetLeft < 150) {
         ++score;
         scoreHtml.innerHTML = score;
     }
